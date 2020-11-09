74327cb1275eeb69883682b341130189775b2cc1
nono5546 <sunghakzz@naver.com>
2020-11-09 22:15:27 +0900

    Init Spring project. (#1)
    * Init Spring project.
    
    * Create README.md
    
    Co-authored-by: Hyeonwoo Park <hyeonwoo.park@kakao.com>

---
+++ src/main/java/com/eskdr/eskadar/EskadarApplication.java
@@ -0,0 +1,13 @@
+package com.eskdr.eskadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskadarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EskadarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eskdr/eskadar/EskadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskdr.eskadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskadarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
